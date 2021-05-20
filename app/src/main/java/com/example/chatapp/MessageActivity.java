5211b30420324fae2b7d84ee49ed6c7b22c6bfee
Vaibhav Ajmera <51504937+vajmera@users.noreply.github.com>
2021-05-20 16:28:49 +0530

    creates a Chat folder in firebase realtime database and saves the sent message(sendersId,receiversID)

--- app/src/main/java/com/example/chatapp/MessageActivity.java
+++ app/src/main/java/com/example/chatapp/MessageActivity.java
@@ -6,7 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
@@ -21,6 +25,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -30,6 +36,10 @@ public class MessageActivity extends AppCompatActivity {
 
     FirebaseUser fuser;
     DatabaseReference reference;
+
+
+    ImageButton btn_send;
+    EditText text_send;
 
     Intent intent;
     @Override
@@ -52,12 +62,31 @@ public class MessageActivity extends AppCompatActivity {
 */
         profile_image=findViewById(R.id.profile_image);
         username=findViewById(R.id.username);
+        btn_send=findViewById(R.id.btn_send);
+        text_send=findViewById(R.id.text_send);
 
         intent=getIntent();
         //getting object from another activity
         String userid=intent.getStringExtra("userId");
-
         fuser= FirebaseAuth.getInstance().getCurrentUser();
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg=text_send.getText().toString();
+                if(!msg.equals(""))
+                {
+                    sendMessage(fuser.getUid(),userid,msg);
+                }
+                else{
+                    Toast.makeText(MessageActivity.this,"Yoy Can't send empty messsage ",Toast.LENGTH_SHORT).show();
+
+                }
+
+                text_send.setText("");
+            }
+        });
+
+
         reference= FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
         //Classes implementing this interface can be used to receive
@@ -87,5 +116,19 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void sendMessage(String sender,String receiver,String message)
+    {
+        DatabaseReference reference=FirebaseDatabase.getInstance().getReference();
+        HashMap<String,Object> hashMap=new HashMap<>();
+
+        hashMap.put("sender",sender);
+        hashMap.put("receiver",receiver);
+        hashMap.put("message",message);
+        //creates a Chat folder in firebase realtime database and saves the
+        //message sent, and sender's userId and receiver's UserID
+        reference.child("Chats").push().setValue(hashMap);
+
     }
 }
